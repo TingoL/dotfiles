f2ae4888279f87db62cb6df23b68d6a5bd651a7a
tingol <amelxmx@gmail.com>
2015-07-15 15:25:38 +0200

    cool new scripts

---
+++ bin/tmux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tmux new-session -d
+tmux rename-session 'home'
+tmux rename-window 'work'
+tmux clock-mode
+tmux split-window -h -p 80
+tmux select-pane -t 0
+tmux split-window -v -p 80 'hnb'
+tmux new-window -n 'reddit' 'rtv -u t1ngol'
+tmux new-window -n 'chat' 'cd $HOME/Dropbox/Stuff/Dropballs/ && python2 test1.py tingol && cd -'
+tmux split-window -v 'weechat-curses'
+tmux new-window -n 'news' 'newsbeuter -q'
+tmux split-window -v 'mutt'
+tmux selectw -t 1
+tmux -2 attach-session -t home
